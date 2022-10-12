dca370055c5dced84079e80380f62afa7b0c136d
Sebastian Avendaño <sebastian.avendano.mt@usa.edu.co>
2022-10-12 08:12:06 -0500

    admin

--- src/main/resources/static/js/funciones.js
+++ src/main/resources/static/js/funciones.js
@@ -369,3 +369,65 @@ function pintarRespuestaScore(items) {
     $("#resultadoScore").append(myTable);
 
 }
+
+/*__________
+Admin
+__________*/
+
+function guardarInformacionAdmin() {
+
+    $("#resultadoAdmin").empty();
+    let myData = { email: $("#emailAdmin").val(), password: $("#passwordAdmin").val(), name: $("#nameAdmin").val(), age: $("#ageAdmin").val(), }
+    let dataToSend = JSON.stringify(myData);
+
+    $.ajax({
+        url: BASE_URL + '/api/Admin/save',
+        type: 'POST',
+        data: dataToSend,
+        datatype: "JSON",
+        contentType: 'application/json',
+        success: function (respuesta) {
+            console.log(respuesta);
+            alert("Inserción exitosa");
+        },
+        error: function (xhr, status) {
+            alert('Operacion no satisfactoria,' + xhr.status);
+        }
+    }
+    );
+}
+
+function traerInformacionAdmin() {
+    $.ajax({
+        url: BASE_URL + '/api/Admin/all',
+        type: "GET",
+        datatype: "JSON",
+        success: function (respuesta) {
+            pintarRespuestaAdmin(respuesta);
+
+        }
+    }
+    );
+}
+
+function pintarRespuestaAdmin(items) {
+
+    $("#resultadoAdmin").empty();
+
+    let myTable = "<table border=3>";
+    myTable += "<tr><th>Id</th><th>Email</th><th>Password</th><th>Name</th></tr>";
+    for (i = 0; i < items.length; i++) {
+        myTable += "<tr>";
+        myTable += "<td>" + items[i].idAdmin + "</td>";
+        myTable += "<td>" + items[i].email + "</td>";
+        myTable += "<td>" + items[i].password + "</td>";
+        myTable += "<td>" + items[i].name + "</td>";
+        //myTable+="<td><button onclick='borrarAdmin("+items[i].id+")'>Borrar</button>";
+        //myTable+="<td><button onclick='actualizarAdmin("+items[i].id+")'>Actualizar</button>";
+        myTable += "</tr>";
+
+    }
+    myTable += "</table>";
+    $("#resultadoAdmin").append(myTable);
+
+}
