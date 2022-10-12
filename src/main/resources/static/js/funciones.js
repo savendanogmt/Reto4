1089b1d1da0f1c2c724b8ff07da7bfadcc53f25a
Sebastian Avendaño <sebastian.avendano.mt@usa.edu.co>
2022-10-12 18:35:17 -0500

    js motorbike

--- src/main/resources/static/js/funciones.js
+++ src/main/resources/static/js/funciones.js
@@ -243,6 +243,8 @@ function guardarInformacionMotorbike() {
         datatype: "JSON",
         contentType: 'application/json',
         success: function (respuesta) {
+            limpiarMotorbike();
+            traerInformacionMotorbike();
             console.log(respuesta);
             alert("Inserción exitosa");
         },
@@ -279,14 +281,65 @@ function pintarRespuestaMotorbike(items) {
         myTable += "<td>" + items[i].year + "</td>";
         myTable += "<td>" + items[i].description + "</td>";
         myTable += "<td>" + items[i].name + "</td>";
-        //myTable+="<td><button onclick='deleteMotorbike("+items[i].id+")'>delete</button>";
-        //myTable+="<td><button onclick='updateMotorbike("+items[i].id+")'>update</button>";
+        myTable+="<td><button onclick='borrarMotorbike("+items[i].id+")'>Borrar</button>";
+        myTable+="<td><button onclick='actualizarMotorbike("+items[i].id+")'>Actualizar</button>";
         myTable += "</tr>";
 
     }
     myTable += "</table>";
     $("#resultadoMotorbike").append(myTable);
 
+}
+
+
+function borrarMotorbike(idElemento) {
+
+    $.ajax({
+        url: BASE_URL + '/api/Motorbike/' + idElemento,
+        type: 'DELETE',
+        datatype: "JSON",
+        success: function (respuesta) {
+            // console.log(respuesta);
+            traerInformacionMotorbike();
+            alert("Borrado exitoso");
+        },
+        error: function (xhr, status) {
+            alert('Operacion no satisfactoria,' + xhr.status);
+        }
+    }
+    );
+}
+
+function actualizarMotorbike(idElemento) {
+
+    $("#resultado").empty();
+    
+    let myData = { id: idElemento, brand: $("#brandMotorbike").val(), year: $("#year_bMotorbike").val(), description: $("#descriptionMotorbike").val(), name: $("#nameMotorbike").val()  }
+    let dataToSend = JSON.stringify(myData);
+
+    $.ajax({
+        url: BASE_URL + '/api/Motorbike/update',
+        type: 'PUT',
+        data: dataToSend,
+        datatype: "JSON",
+        contentType: 'application/json',
+        success: function (respuesta) {
+            traerInformacionMotorbike();
+            limpiarMotorbike();
+            alert("Actualizacion exitosa");
+        },
+        error: function (xhr, status) {
+            alert('Operacion no satisfactoria,' + xhr.status);
+        }
+    }
+    );
+}
+
+function limpiarMotorbike() {
+    $("#brandMotorbike").val("");
+    $("#year_bMotorbike").val("");
+    $("#descriptionMotorbike").val("");
+    $("#nameMotorbike").val("");
 }
 
 /*__________
