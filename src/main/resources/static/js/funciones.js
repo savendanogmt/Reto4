2a381432d05e5e89c94eb7b95742522fdc8dcddd
Sebastian Avendaño <sebastian.avendano.mt@usa.edu.co>
2022-10-12 01:10:56 -0500

    js and index score

--- src/main/resources/static/js/funciones.js
+++ src/main/resources/static/js/funciones.js
@@ -1,6 +1,6 @@
 
-const BASE_URL = 'http://152.70.143.130:8080'
-//const BASE_URL = 'http://localhost:8080'
+//const BASE_URL = 'http://152.70.143.130:8080'
+const BASE_URL = 'http://localhost:8080'
 /*__________
 Categoria
 __________*/
@@ -306,5 +306,66 @@ function pintarRespuestaReservation(items) {
     }
     myTable += "</table>";
     $("#resultadoReservation").append(myTable);
+
+}
+
+/*__________
+Score
+__________*/
+
+function guardarInformacionScore() {
+
+    $("#resultadoScore").empty();
+    let myData = { stars: $("#stars").val(), messageText: $("#messageText").val(), }
+    let dataToSend = JSON.stringify(myData);
+
+    $.ajax({
+        url: BASE_URL + '/api/Score/save',
+        type: 'POST',
+        data: dataToSend,
+        datatype: "JSON",
+        contentType: 'application/json',
+        success: function (respuesta) {
+            console.log(respuesta);
+            alert("Inserción exitosa");
+        },
+        error: function (xhr, status) {
+            alert('Operacion no satisfactoria,' + xhr.status);
+        }
+    }
+    );
+}
+
+function traerInformacionScore() {
+    $.ajax({
+        url: BASE_URL + '/api/Score/all',
+        type: "GET",
+        datatype: "JSON",
+        success: function (respuesta) {
+            pintarRespuestaScore(respuesta);
+
+        }
+    }
+    );
+}
+
+function pintarRespuestaScore(items) {
+
+    $("#resultadoScore").empty();
+
+    let myTable = "<table border=3>";
+    myTable += "<tr><th>Id</th><th>Stars</th><th>Message</th></tr>";
+    for (i = 0; i < items.length; i++) {
+        myTable += "<tr>";
+        myTable += "<td>" + items[i].idScore + "</td>";
+        myTable += "<td>" + items[i].stars + "</td>";
+        myTable += "<td>" + items[i].messageText + "</td>";
+        //myTable+="<td><button onclick='borrarScore("+items[i].id+")'>Borrar</button>";
+        //myTable+="<td><button onclick='actualizarScore("+items[i].id+")'>Actualizar</button>";
+        myTable += "</tr>";
+
+    }
+    myTable += "</table>";
+    $("#resultadoScore").append(myTable);
 
 }
